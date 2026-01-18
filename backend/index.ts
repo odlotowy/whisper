910891a58fae316c8bad378a27af7f893f9fb5a2
Antoni <antiolos17@gmail.com>
2026-01-18 23:09:52 +0100

    Refactor database connection and enhance User model to make avatar field required

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,15 @@
-import app from './src/app';
-import { connectDB } from './src/config/database';
+import app from "./src/app";
+import { connectDB } from "./src/config/database";
 
 const PORT = process.env.PORT || 3000;
 
-connectDB().then(() => {
-	app.listen(PORT, () => {
-		console.log(`API is running on http://localhost:${PORT}`);
-	});
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`API is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Startup error:", error);
+    process.exit(1);
+  });
