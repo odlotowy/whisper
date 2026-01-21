1c98a98fb3d0864dfe4175bc2cef1eca82c87260
Antoni <antiolos17@gmail.com>
2026-01-21 13:43:47 +0100

    Refactor socket user ID handling and update message sender population

--- backend/src/utils/socket.ts
+++ backend/src/utils/socket.ts
@@ -5,10 +5,6 @@ import { Message } from "../models/Message";
 import { Chat } from "../models/Chat";
 import { User } from "../models/User";
 
-interface SocketWithUserId extends Socket {
-  userId: string;
-}
-
 // store online users in memory: userId -> socketId
 export const onlineUsers: Map<string, string> = new Map();
 
@@ -16,8 +12,8 @@ export const initializeSocket = (httpServer: HttpServer) => {
   const allowedOrigins = [
     "http://localhost:8081", // Expo mobile
     "http://localhost:5187", // Vite web dev
-    process.env.FRONTEND_URL as string, // production
-  ];
+    process.env.FRONTEND_URL, // production
+  ].filter(Boolean) as string[];
 
   const io = new SocketServer(httpServer, { cors: { origin: allowedOrigins } });
 
@@ -37,7 +33,7 @@ export const initializeSocket = (httpServer: HttpServer) => {
       const user = await User.findOne({ clerkId });
       if (!user) return next(new Error("User not found"));
 
-      (socket as SocketWithUserId).userId = user._id.toString();
+      socket.data.userId = user._id.toString();
 
       next();
     } catch (error: any) {
@@ -48,7 +44,7 @@ export const initializeSocket = (httpServer: HttpServer) => {
   // this "connection" event name is special and should be written like this
   // it's the event that is triggered when a new client connects to the server
   io.on("connection", (socket) => {
-    const userId = (socket as SocketWithUserId).userId;
+    const userId = socket.data.userId;
 
     // send list of currently online users to the newly connected client
     socket.emit("online-users", { userIds: Array.from(onlineUsers.keys()) });
@@ -96,7 +92,7 @@ export const initializeSocket = (httpServer: HttpServer) => {
           chat.lastMessageAt = new Date();
           await chat.save();
 
-          await message.populate("sender", "name email avatar");
+          await message.populate("sender", "name avatar");
 
           // emit to chat room (for users inside the chat)
           io.to(`chat:${chatId}`).emit("new-message", message);
